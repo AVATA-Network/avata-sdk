2d378a6fd5825fcb4549336fe6f607f7bd5db0e5
AVATA-Network <Hello@avata.network>
2022-06-01 11:11:30 +0700

    chore: initial commit

---
+++ src/constants.ts
@@ -0,0 +1,53 @@
+import JSBI from 'jsbi'
+
+// exports for external consumption
+export type BigintIsh = JSBI | bigint | string
+
+export enum ChainId {
+  RINKEBY = 4,
+  FUJI = 43113,
+  AVALANCHE = 43114
+}
+
+export enum TradeType {
+  EXACT_INPUT,
+  EXACT_OUTPUT
+}
+
+export enum Rounding {
+  ROUND_DOWN,
+  ROUND_HALF_UP,
+  ROUND_UP
+}
+
+export const INIT_CODE_HASH = '9e29d2dd33c19ebaee1d19e757d366fc6e57665322d7a389d300924dd9ebb39d'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const TWO = JSBI.BigInt(2)
+export const THREE = JSBI.BigInt(3)
+export const FIVE = JSBI.BigInt(5)
+export const TEN = JSBI.BigInt(10)
+export const _100 = JSBI.BigInt(100)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
+
+export enum SolidityType {
+  uint8 = 'uint8',
+  uint256 = 'uint256'
+}
+
+export const FACTORY_ADDRESS: { [chainId in ChainId]: string } = {
+  [ChainId.RINKEBY]: '0x0000000000000000000000000000000000000000',
+  [ChainId.FUJI]: '0xA864D122a76f9Eb86BC6a34eC92D1c3010B4aaE3',
+  [ChainId.AVALANCHE]: '0x0000000000000000000000000000000000000000'
+}
+
+
+export const SOLIDITY_TYPE_MAXIMA = {
+  [SolidityType.uint8]: JSBI.BigInt('0xff'),
+  [SolidityType.uint256]: JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+}
